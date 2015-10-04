0cf00f477230eef46cf91d03b0d79e1da447b512
XingJingxuan <xjx524@gmail.com>
2015-10-04 13:39:20 +0800

    lesson 8

--- js/app.js
+++ js/app.js
@@ -1,9 +1,17 @@
-window.onload = function() {
-  console.log("page loaded");
-  makeCartScrollNicely();
+window.onload = function () {
+    console.log("page loaded");
+    makeCartScrollNicely();
+
+    var $toggle = document.querySelector(".site__right-sidebar-toggle");
+    $toggle.addEventListener("click", function () {
+        document.body.classList.toggle("js-show-right-sidebar");
+        document.querySelector(".site__right-sidebar").classList.toggle("max-width-600-hide");
+    });
 };
 
 function makeCartScrollNicely() {
-  var cart = document.querySelector(".cart");
-  Ps.initialize(cart);
+    var cart = document.querySelector(".cart");
+    var main = document.querySelector(".site__main");
+    Ps.initialize(cart);
+    Ps.initialize(main);
 }
